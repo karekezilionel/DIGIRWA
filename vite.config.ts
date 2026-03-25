918602b5013f6a40cfe25b355ccc1850c872e7e6
karekezilionel <l.karekezi@alustudent.com>
2026-03-25 14:03:02 -0700

    new updates

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,21 @@ import { defineConfig } from "vite";
 import path from "path";
 
 export default defineConfig({
-  base: "./",   
+  base: "./",
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+        login: path.resolve(__dirname, "login.html"),
+        register: path.resolve(__dirname, "register.html"),
+      },
+    },
+  },
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "."),
     },
-  },
-
-  server: {
-    hmr: process.env.DISABLE_HMR !== "true",
   },
 });
