2d2ea13394ae80cd8c796f3c96842eafb9da2c32
karekezilionel <l.karekezi@alustudent.com>
2026-03-25 13:43:42 -0700

    newly updated codes

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from "vite";
 import path from "path";
 
 export default defineConfig({
+  base: "./",   
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "."),
     },
   },
+
   server: {
     hmr: process.env.DISABLE_HMR !== "true",
   },
